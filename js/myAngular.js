a57baa45a67c60a13a1571318b87ea20472e1fe0
MrArkais <q.jeannot@outlook.fr>
2019-01-31 22:28:09 +0100

    Ajout sur Code Moha des routes+ multiples controlleurs

--- js/myAngular.js
+++ js/myAngular.js
@@ -1,24 +1,56 @@
-var myAngular = angular.module('plunker', ['selectize']);
+var myAngular = angular.module('app', ['selectize','ngRoute']);
+//===================
+//Factory
+//===================
 
-myAngular.controller('MainCtrl', function($scope, $http, $sce) {
-    $scope.disable = false;
-    //=======================================================
-    //Angular Form Bindings
-    //=======================================================
-    $scope.myModel;
 
-    $scope.myConfig = {
-        create: true,
-        onChange: function(value){
-            console.log('onChange', value)
-        },
-        // maxItems: 1,
-        // required: true,
-    }
 
-    var ing = '';
-    $scope.list = [];
-    $scope.text = '';
+
+
+
+// configurer les routes
+myAngular.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+
+    $routeProvider
+
+    // route pour la page index
+        .when('/', {
+            templateUrl : 'pages/multiplesearch.html',
+            controller  : 'Multiple'
+        })
+
+        // route pour la page recherche simple
+        .when('/simple', {
+            templateUrl : 'pages/singlesearch.html',
+            controller  : 'Single'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+});
+
+
+//=======================================
+//Fin routing
+//=======================================
+
+//=======================================
+//Controleur Carroussel
+//=======================================
+myAngular.controller('printall', function ($scope, $http) {
+
+/*    $scope.$on('topic', function (event, arg) {
+        $scope.receiver = 'got your ' + arg;
+    });
+
+    $scope.$on('length', function (event,arg) {
+        $scope.length = arg;
+
+    })*/
 
     var uriAllRecipe = "http://localhost:9090/recipes/recipes";
     $scope.allRecipe = [];
@@ -37,35 +69,68 @@ myAngular.controller('MainCtrl', function($scope, $http, $sce) {
             $scope.lengthAllRecipe = response.data.length;
         });
 
-    /*Changer de mode de recherche*/
-    $scope.searchMode = 0;
+})
 
-    $scope.doListSearch = function()
-    {
-        $scope.searchMode = 1;
-    };
+//=======================================
+//Controleur Resultat
+//=======================================
+myAngular.controller('Result', function ($scope) {
 
-    $scope.doIngSearch = function() {
-        $scope.searchMode = 0;
+    $scope.$on('print', function (event, arg) {
+        $scope.data = arg;
+
+    })
+
+    $scope.$on('length', function (event,arg) {
+        $scope.length = arg;
+
+    })
+})
+
+//=======================================
+//Controleur Recherche Simple
+//=======================================
+myAngular.controller('Single', function($rootScope,$scope, $http) {
+    $scope.disable = false;
+
+    console.log("Single");
+
+    //=======================================================
+    //Angular Form Bindings
+    //=======================================================
+    $scope.myModel;
+
+
+    $scope.myConfig = {
+        create: true,
+        onChange: function(value){
+            console.log('onChange', value)
+        },
+        // maxItems: 1,
+        // required: true,
     }
+
+
+    $scope.list = [];
+    $scope.text = '';
 
     $scope.submit = function()
     {
+
         if ($scope.myModel != "" && $scope.myModel != null)
         {
+
             $scope.ingredient= $scope.myModel.toString();
+
 
             console.log($scope.ingredient);
 
-            if($scope.searchMode == 0) {
-                var completeURL =  "http://localhost:9090/recipes/find/"+$scope.ingredient;
-            }
-            else  {
-                var completeURL = "http://localhost:9090/recipes/findM/"+$scope.ingredient;
-            }
+            var completeURL = "http://localhost:9090/recipes/find/"+$scope.ingredient;
+
 
             $http.get(completeURL)
                 .then(function(response) {
+
                     console.log(response.data.length);
                     if (response.data.length == 0)
                     {
@@ -75,17 +140,106 @@ myAngular.controller('MainCtrl', function($scope, $http, $sce) {
                         $scope.data = response.data;
                         console.log($scope.data);
                     }
+
                     $scope.length = response.data.length;
+
+                    $rootScope.$broadcast('length', $scope.length);
+                    $rootScope.$broadcast('print', $scope.data);
                 });
+
+
         }
         else {
             $scope.data = null;
             $scope.empty = true;
             $scope.length = 0;
         }
+
+
+
     };
 
-    $scope.to_trusted = function(html_code) {
-        return $sce.trustAsHtml(html_code);
-    }
+
+
 });
+
+
+
+//=======================================
+//Controleur Recherche Multiple
+//=======================================
+myAngular.controller('Multiple', function($scope, $http, $rootScope) {
+    $scope.disable = false;
+
+    console.log("Multiple");
+
+
+
+    //=======================================================
+    //Angular Form Bindings
+    //=======================================================
+    $scope.myModel;
+
+
+    $scope.myConfig = {
+        create: true,
+        onChange: function(value){
+            console.log('onChange', value)
+        },
+        // maxItems: 1,
+        // required: true,
+    }
+
+
+    $scope.list = [];
+    $scope.text = '';
+
+    $scope.submit = function()
+    {
+
+        if ($scope.myModel !== "" && $scope.myModel != null)
+        {
+
+            $scope.ingredient= $scope.myModel.toString();
+
+
+            console.log($scope.ingredient);
+
+                var completeURL = "http://localhost:9090/recipes/findM/"+$scope.ingredient;
+
+
+            $http.get(completeURL)
+                .then(function(response) {
+
+                    console.log("je suis la");
+                    if (response.data.length === 0)
+                    {
+                        $scope.data = null;
+                    }
+                    else {
+                        $scope.data = response.data;
+                        console.log($scope.data);
+                    }
+
+                    $scope.length = response.data.length;
+
+                    $rootScope.$broadcast('length', $scope.length);
+                    $rootScope.$broadcast('print', $scope.data);
+                });
+
+
+        }
+        else {
+            $scope.data = null;
+            $scope.empty = true;
+            $scope.length = 0;
+        }
+
+
+    };
+
+
+
+});
+
+
